bc2c0ca9c861b4f3454ed970e7c65c6e077c5654
Rafa Ulinnuha <ulinnuharafaazka@gmail.com>
2021-05-16 09:35:27 +0700

    mengupdate pengunjung 2

---
+++ listKamar.hpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+void listKamar(){
+    system("cls");
+    std::cout << "\t\t\tLIST KAMAR\n";
+    std::cout << "\n";
+    std::cout << "1. Kamar A\n";
+    std::cout << "Fasilitas\t\t: Max. 2 orang, Wifi, SPA, Kolam\n";
+    std::cout << "\t\t\t  Renang, Sarapan, AC\n";
+    std::cout << "Harga/malam\t\t: Rp. 400.000,00\n";
+    std::cout << "Jumlah kamar tersedia\t: "; // fungsi hitung kamar A
+    std::cout << "\n\n2. Kamar B\n";
+    std::cout << "Fasilitas\t\t: Max. 4 orang, Wifi, AC\n";
+    std::cout << "Harga/malam\t\t: Rp. 400.000,00\n";
+    std::cout << "Jumlah kamar tersedia\t: "; // fungsi hitung kamar B
+}
--- pengunjung.hpp
+++ pengunjung.hpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "pesanKamar.hpp"
+#include "listKamar.hpp"
 
 void menuPengunjung(){
     system("cls");
@@ -14,22 +15,15 @@ void menuPengunjung(){
     std::cin >> tamuPilih;
     switch(tamuPilih){
         case 1:
-            system("cls");
-            std::cout << "\t\t\tLIST KAMAR\n";
-            std::cout << "\n";
-            std::cout << "1. Kamar A\n";
-            std::cout << "Fasilitas\t\t: Max. 2 orang, Wifi, SPA, Kolam\n";
-            std::cout << "\t\t\t  Renang, Sarapan, AC\n";
-            std::cout << "Harga/malam\t\t: Rp. 400.000,00\n";
-            std::cout << "Jumlah kamar tersedia\t: "; // fungsi hitung kamar A
-            std::cout << "\n\n2. Kamar B\n";
-            std::cout << "Fasilitas\t\t: Max. 4 orang, Wifi, AC\n";
-            std::cout << "Harga/malam\t\t: Rp. 400.000,00\n";
-            std::cout << "Jumlah kamar tersedia\t: "; // fungsi hitung kamar B
+            listKamar();
             std::cout << "\n\nBack to menu (Y/N)? ";
             std::cin >> pilih;
             if (pilih == 'Y' || pilih == 'y'){
                 menuPengunjung();
+            } else if (pilih == 'N' || pilih == 'n'){
+                exit(0);
+            } else {
+                std::cout << "Masukkan Anda Salah!!\n";
             }
             break;
         case 2:
